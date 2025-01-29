61a1c3cc12bc53cddd350b159668e8a664b3c4e5
MOUHAMED172002 <abdoul172002@gmail.com>
2025-01-29 11:43:47 +0100

    initialisation des variables

---
+++ script.js
@@ -0,0 +1,21 @@
+var = [
+    {
+        question: "C/'est quoi l/'informatique ?" ,
+        answers :["Science du traitement automatique de l'information","Englobe les 2 mots « information et automatique »","Science du traitement manuel de l/'information"],
+        correct : "2",
+    },
+    {
+        question: "C/'est quoi l/'informatique ?",
+        answers :["Science du traitement automatique de l'information","Englobe les 2 mots « information et automatique »","Science du traitement manuel de l/'information"],
+        correct : "1",
+    },
+    {
+        question: "C/'est quoi l/'informatique ?",
+        answers :["Science du traitement automatique de l'information","Englobe les 2 mots « information et automatique »","Science du traitement manuel de l/'information"],
+        correct : "3",
+    }
+];
+
+var quiz = document.getElementById('quiz');
+var btn = document.getElementById('button');
+var resultat = document.getElementById('resultat');
